696e765c2aa3bd9d8e7c942cc2d88aea496c2b77
sami-theion <sami@theion.de>
2022-08-05 11:45:24 +0200

    run sub

--- includes/keyence_server.h
+++ includes/keyence_server.h
@@ -15,6 +15,8 @@
 #include <string>
 #include "sockpp/tcp_acceptor.h"
 #include <stdlib.h>
+#include "distance_sensor_interface.h"
+
 class keyence_server
 {
 private:
@@ -25,7 +27,13 @@ private:
     uint16_t PORT=6555;
     const int DATA_LENGTH =1024;
     char Response[1024]={};
+    std::string Request;
     int incomingData=0;
+    std::map<std::string, std::map<std::string, uint16_t> > ip_port_map;
+
+    const char* keyence_controller_ip = "192.168.0.104"; //ip controller
+    uint16_t keyence_controller_port = 24687; // port
+    IdistanceSensor* KeyenceDistSensor;
 public:
     keyence_server();
     keyence_server(const char* ip, uint16_t port);
@@ -33,6 +41,11 @@ public:
 
     /**** server methods ***/
     void server_connect();
+    void server_parse();
+    void create_sensor();
+    void init_sensor();
+    void send_cmd();
+    double get_value();
 };
 
 
--- src/keyence_server.cpp
+++ src/keyence_server.cpp
@@ -20,7 +20,11 @@ keyence_server::keyence_server(const char* ip, uint16_t port)
 keyence_server::keyence_server()
 {
     std::cout << "creating keyence server with default ip: " <<this->IP<< ", port: "<<this->PORT<< std::endl;
-}
+
+    ip_port_map["oscilloscop"]["192.168.0.105"]=3000;
+    ip_port_map["delta"]["192.168.0.201"]=3344;
+    ip_port_map["keyence"]["192.168.0.104"]=24687;
+	}
 keyence_server::~keyence_server()
 {
     delete ServObject;
@@ -30,7 +34,6 @@ keyence_server::~keyence_server()
 void keyence_server::server_connect()
 {
 	ServObject = new sockpp::tcp_acceptor(PORT);
-
 	if (!ServObject) {
 		std::cerr << "Error creating the server: " << ServObject->last_error_str() << std::endl;
 		return;
@@ -39,22 +42,32 @@ void keyence_server::server_connect()
 
 	while (true) {
 		sockpp::inet_address peer;
-
 		// Accept a new client connection
 		sockpp::tcp_socket sock = ServObject->accept(&peer);
 		std::cout << "Received a connection request from " << peer << std::endl;
-
 		if (!sock) {
 			std::cerr << "Error accepting incoming connection: " 
 				<< ServObject->last_error_str() << std::endl;
 		}
-		else {
-			// Create a thread and transfer the new stream to it.
-			//thread thr(run_echo, std::move(sock));
-			//thr.detach();
-		}
+        // Read data from keyence
+        ssize_t n = sock.read(&Request[0], DATA_LENGTH);
+        std::cout << "n bytes: " << n << std::endl;
+        std::cout << "data received: "<<Request.c_str() << std::endl;		
+		sock.write(Request.c_str() , DATA_LENGTH);
+		std::cout << "data sent: "<<Request.c_str() << std::endl;		
+
 	}
+}
 
-
-
+void keyence_server::create_sensor ()
+{
+    KeyenceDistSensor= new IdistanceSensor (1, new keyence::keyenceWinSocket(this->keyence_controller_ip, this->keyence_controller_port)); // instance sensor
+}
+void keyence_server::init_sensor ()
+{
+    KeyenceDistSensor->initSensorCom(); // init sensor communication
+}
+double keyence_server::get_value ()
+{
+    return KeyenceDistSensor->getCurrentDistance(); // get sensor mesured data
 }
